df6fdf1f73aec70057fef7b8ebb0d9e377540276
rohitsrivastava99@gmail.com <rohitsrivastava99@gmail.com>
2016-05-13 23:22:02 +0530

    added

--- about.php
+++ about.php
@@ -5,8 +5,12 @@
 		<div class="about-grids">
 			<div class="about-left">
 				<h3>WHO WE ARE</h3>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages. </p>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,</p>
+				<p>We provide you such a facility which you and your friends have been loking for. We provide you a sms and calling alert facility, by which you can do your work in no time and save your time.
+
+For those who want to alert or invite their all friends to their party or make their friends aware about their new contact numbers.
+
+We provide you a message and calling facility with a finding facility that helps your friends to find from your old contact to your new contact number without sing in us </p>
+				
 				
 			</div>
 			<div class="about-right">
